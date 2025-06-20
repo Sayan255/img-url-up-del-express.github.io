28192be50d3a8526ae9b83f43c06962ae17798e7
Sayan Datta <sayandatta2001@gmail.com>
2025-06-20 15:50:15 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(`mongodb://127.0.0.1:27017/testapp`);
+mongoose.connect(`mongodb://localhost:27017/testapp`);
 
 const userSchema = new mongoose.Schema({
   name: String,
